a5170fa576b954d9ed0856f8672fad0ae475c82b
Jess Muir <e.jessmuir@gmail.com>
2022-07-13 00:57:15 +0000

    Go install working

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/bash
 # install go if required
 if which go; then
+    echo go installed, continuing
 else
     wget https://go.dev/dl/go1.18.4.linux-amd64.tar.gz
-    tar -C /usr/local -xzf go1.18.4.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xzf go1.18.4.linux-amd64.tar.gz
+    echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
+    # extra export just for the duration of the script
+    export PATH=$PATH:/usr/local/go/bin
 fi
