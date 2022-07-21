8032d729ebfb5643e95576e1f28ca75385983ce7
Jess Muir <e.jessmuir@gmail.com>
2022-07-21 11:59:31 -0400

    Clone instead of build remote :(

--- cmd/myops/git.go
+++ cmd/myops/git.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/storage/memory"
 )
+
+const clonePath string = "cloneTemp"
 
 const shortHashSize int = 7
 
@@ -28,4 +41,55 @@ func remoteShorthash(repoUrl string, branch string) string {
 	}
 
 	return ""
+}
+
+func clone(repoUrl string, branch string) *bytes.Buffer {
+	fmt.Println("cloning", repoUrl+"#"+branch)
+	git.PlainClone(clonePath, false, &git.CloneOptions{
+		URL:           repoUrl,
+		ReferenceName: plumbing.NewBranchReferenceName(branch),
+		SingleBranch:  true,
+	})
+
+	defer os.RemoveAll(clonePath)
+
+	ret := &bytes.Buffer{}
+
+	gzw := gzip.NewWriter(ret)
+	defer gzw.Close()
+
+	tw := tar.NewWriter(gzw)
+	defer tw.Close()
+
+	filepath.Walk(clonePath, func(file string, fi fs.FileInfo, err error) error {
+		if err != nil {
+			return (err)
+		}
+
+		header, err := tar.FileInfoHeader(fi, fi.Name())
+		if err != nil {
+			return (err)
+		}
+
+		header.Name = strings.TrimPrefix(strings.Replace(file, clonePath, "", -1), string(filepath.Separator))
+
+		if err := tw.WriteHeader(header); err != nil {
+			return (err)
+		}
+
+		f, err := os.Open(file)
+		if err != nil {
+			return (err)
+		}
+
+		if _, err := io.Copy(tw, f); err != nil {
+			return err
+		}
+
+		f.Close()
+
+		return nil
+	})
+
+	return ret
 }
--- cmd/myops/main.go
+++ cmd/myops/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	cli "github.com/docker/docker/client"
 )
@@ -23,14 +24,22 @@ func update() {
 	cleanupVolumes(ctx, client, configs)
 
 	for projectName, config := range configs {
-		shortHash := remoteShorthash(config.RepoUrl, config.Branch)
-		projectTag := projectName + ":" + shortHash
+		// shortHash := remoteShorthash(config.RepoUrl, config.Branch)
+		// projectTag := projectName + ":" + shortHash
 
-		buildImage(ctx, client, config, projectName)
-		err := runContainer(ctx, client, config, projectName, projectTag)
+		fmt.Println("cloning", projectName)
+		tarBuffer := clone(config.RepoUrl, config.Branch)
+		file, err := os.Create(clonePath)
 		if err != nil {
 			panic(err)
 		}
+		file.Write(tarBuffer.Bytes())
+
+		// buildImage(ctx, client, config, projectName)
+		// err := runContainer(ctx, client, config, projectName, projectTag)
+		// if err != nil {
+		// 	panic(err)
+		// }
 	}
 
 	renderCaddyfile(configs)
